b31188b542389460e5dbca3fb6d85091f745064c
MJafar Mashhadi <raindigital2007@gmail.com>
2016-07-08 00:40:03 +0430

    Initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    version='0.0.1',
+    name='nivad',
+    description='Nivad server-server api package',
+    url='https://github.com/NivadCloud/NivadPy',
+    author='MJafar Mashhadi',
+    author_email='mjafar@nivad.io',
+    license='GPL',
+    packages=['nivad'],
+    zip_safe=False
+)
